d1d339973dbfb759e996d37e0b69d2800b26450a
Alex On <shuhia.on@gmail.com>
2021-09-07 21:07:29 +0200

    implement quiz form

--- src/components/NewQuizForm.js
+++ src/components/NewQuizForm.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4, v4 } from "uuid";
 import ROUTES from "../app/routes";
+import { selectTopics } from "../features/topics/topicsSlice";
+import { thunkQuizCreator } from "../features/quizzes/quizzesSlice";
+import { addCard } from "../features/cards/cardSlice";
 
 export default function NewQuizForm() {
   const [name, setName] = useState("");
   const [cards, setCards] = useState([]);
   const [topicId, setTopicId] = useState("");
   const history = useHistory();
-  const topics = {};
+  const topics = useSelector(selectTopics);
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,8 +24,13 @@ export default function NewQuizForm() {
     const cardIds = [];
 
     // create the new cards here and add each card's id to cardIds
+    cards.forEach((card) => {
+      const id = v4();
+      cardIds.push(id);
+      dispatch(addCard({ id, ...card }));
+    });
     // create the new quiz here
-
+    dispatch(thunkQuizCreator({ id: v4(), name, topicId, cardIds }));
     history.push(ROUTES.quizzesRoute());
   };
 
