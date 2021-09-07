ee7836552e54cb56d765cb5a89f31bdd120e1e5f
Alex On <shuhia.on@gmail.com>
2021-09-07 21:07:21 +0200

    implement quiz

---
+++ src/features/quizzes/quizzesSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { addQuizId } from "../topics/topicsSlice";
+
+const initialState = {};
+
+export const thunkQuizCreator = (payload) => {
+  return (dispatch) => {
+    dispatch(addQuiz(payload));
+    dispatch(addQuizId(payload));
+  };
+};
+
+const quizzesSlice = createSlice({
+  name: "quizzes",
+  initialState,
+  reducers: {
+    addQuiz(state, action) {
+      const { id, name, topicsId, cardIds } = action.payload;
+      state[id] = { id, name, topicsId, cardIds };
+    }
+  }
+});
+
+export const selectQuizzes = (state) => state.quizzes;
+export const { addQuiz } = quizzesSlice.actions;
+export default quizzesSlice.reducer;
