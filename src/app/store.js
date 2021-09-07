71298276b57c675c6efbca7083918abfed2abe2a
Alex On <shuhia.on@gmail.com>
2021-09-07 21:07:44 +0200

    add reducers to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cardReducer from "../features/cards/cardSlice";
+import quizzesReducer from "../features/quizzes/quizzesSlice";
+import topicsReducer from "../features/topics/topicsSlice";
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    topics: topicsReducer,
+    quizzes: quizzesReducer,
+    cards: cardReducer,
+  },
 });
